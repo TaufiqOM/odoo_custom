1be8846c7ae57f4b20b1f551db12a0f38ae8de11
TaufiqOM <pt.om.taufiq@gmail.com>
2025-06-25 15:10:59 +0700

    Log

--- ITCustom_TearSheet/models/product_template.py
+++ ITCustom_TearSheet/models/product_template.py
@@ -6,5 +6,22 @@ class ProductTemplate(models.Model):
     
     tear_sheet_pdf = fields.Binary(string="Tear Sheet PDF")
     tear_sheet_pdf_filename = fields.Char(string="PDF Filename")
+    tear_sheet_pdf_upload_date = fields.Datetime(string="Upload Date :")
     file_pdf = fields.Binary(string="Tear Sheet PDF")
     file_pdf_filename = fields.Char(string="PDF Filename")
+    file_pdf_upload_date = fields.Datetime(string="Upload Date :")
+
+    @api.model
+    def create(self, vals):
+        if vals.get('tear_sheet_pdf'):
+            vals['tear_sheet_pdf_upload_date'] = fields.Datetime.now()
+        if vals.get('file_pdf'):
+            vals['file_pdf_upload_date'] = fields.Datetime.now()
+        return super(ProductTemplate, self).create(vals)
+
+    def write(self, vals):
+        if 'tear_sheet_pdf' in vals and vals['tear_sheet_pdf']:
+            vals['tear_sheet_pdf_upload_date'] = fields.Datetime.now()
+        if 'file_pdf' in vals and vals['file_pdf']:
+            vals['file_pdf_upload_date'] = fields.Datetime.now()
+        return super(ProductTemplate, self).write(vals)
