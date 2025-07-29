ca099c1e2ac8a4015b3c8190dc65b99442aeeb7a
TaufiqOM <pt.om.taufiq@gmail.com>
2025-07-29 08:43:00 +0700

    v4

--- ITCustom_Appraisal_Template/models/appraisal_template.py
+++ ITCustom_Appraisal_Template/models/appraisal_template.py
@@ -9,14 +9,18 @@ class AppraisalTemplate(models.Model):
         'hr.department', 
         string='Department'
     )
-    skill_type_id = fields.Many2one(
+    skill_types = fields.Many2many(
         'hr.skill.type',
-        string='Skill Type'
+        'appraisal_template_skill_type_rel',
+        'template_id', 'skill_type_id',
+        string='Skill Types'
     )
-    skill_ids = fields.One2many(
+    skills = fields.Many2many(
         'hr.skill',
-        compute='_compute_skill_ids',
-        string='Skills'
+        'appraisal_template_skill_rel',
+        'template_id', 'skill_id',
+        string='Skills',
+        domain="[('skill_type_id', 'in', skill_types)]"
     )
     skill_names = fields.Text(
         compute='_compute_skill_names',
@@ -24,21 +28,10 @@ class AppraisalTemplate(models.Model):
         readonly=True
     )
 
-    @api.depends('skill_type_id')
-    def _compute_skill_ids(self):
-        for record in self:
-            if record.skill_type_id:
-                record.skill_ids = self.env['hr.skill'].search([
-                    ('skill_type_id', '=', record.skill_type_id.id)
-                ])
-            else:
-                record.skill_ids = False
-
-    @api.depends('skill_type_id')
+    @api.depends('skills')
     def _compute_skill_names(self):
         for record in self:
-            if record.skill_type_id and record.skill_type_id.skill_ids:
-                skill_names = record.skill_type_id.skill_ids.mapped('name')
-                record.skill_names = '\n'.join(skill_names)
+            if record.skills:
+                record.skill_names = '\n'.join(record.skills.mapped('name'))
             else:
                 record.skill_names = ''
