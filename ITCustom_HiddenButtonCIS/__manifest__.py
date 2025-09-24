161e1579acfe9bbbde435766628d4f0a37e37ab5
TaufiqOM <pt.om.taufiq@gmail.com>
2025-09-24 07:36:53 +0700

    Barcode 1

---
+++ ITCustom_HiddenButtonCIS/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "PO Hide Incoming Shipment Button",
+    "version": "18.0.1.0.0",
+    "author": "Custom",
+    "website": "",
+    "category": "Purchase",
+    "license": "LGPL-3",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_order_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
